793572a2761dc4bd240497e89235740d0fc9e9d3
shohiebsense <shohieb.ahmad@gmail.com>
2020-01-09 14:45:08 +0700

    abusing textinputlayout and edittext

--- app/src/main/java/com/shohiebsense/edittextcolorabuse/AbusedTextInputLayout.java
+++ app/src/main/java/com/shohiebsense/edittextcolorabuse/AbusedTextInputLayout.java
@@ -1,4 +1,34 @@
 package com.shohiebsense.edittextcolorabuse;
 
-public class AbusedTextInputLayout  {
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.util.AttributeSet;
+import android.view.ContextThemeWrapper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class AbusedTextInputLayout extends TextInputLayout {
+    public AbusedTextInputLayout(@NonNull Context context) {
+        super(new ContextThemeWrapper(context, R.style.AbusedTextInputLayout));
+        setHintTextAppearance(R.style.HintAbusedTextInputLayout);
+        setDefaultHintTextColor(ContextCompat.getColorStateList(context, R.color.color_label));
+    }
+
+    public AbusedTextInputLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(new ContextThemeWrapper(context, R.style.AbusedTextInputLayout));
+        setHintTextAppearance(R.style.HintAbusedTextInputLayout);
+        setDefaultHintTextColor(ContextCompat.getColorStateList(context, R.color.color_label));
+    }
+
+    public AbusedTextInputLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(new ContextThemeWrapper(context, R.style.AbusedTextInputLayout));
+        setHintTextAppearance(R.style.HintAbusedTextInputLayout);
+        setDefaultHintTextColor(ContextCompat.getColorStateList(context, R.color.color_label));
+    }
+
+
 }
